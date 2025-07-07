5de21d6740691cd8ba88294fdd986d6a6e88de99
Jean Marie Vianey RABEMANALINA <vianey.jean@ymail.com>
2025-07-07 15:16:29 +0400

    ajout scroll

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import ScrollToTop from './ScrollToTop';
 import { useAuth } from '@/contexts/AuthContext';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { RealtimeWrapper } from './common/RealtimeWrapper';
@@ -27,6 +28,7 @@ const Layout: React.FC<LayoutProps> = ({ children, requireAuth = false }) => {
         {children || <Outlet />}
       </main>
       <Footer />
+      <ScrollToTop />
     </div>
   );
   
