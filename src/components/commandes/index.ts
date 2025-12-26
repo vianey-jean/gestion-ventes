cb2d8d5288dffb0946d77db76de4a470f79d9577
Jean Marie Vianey RABEMANALINA <vianey.jean@ymail.com>
2025-12-26 07:48:58 +0400

    mise a jopur page commandes

---
+++ src/components/commandes/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Index des composants pour la page Commandes
+ */
+export { default as CommandesHero } from './CommandesHero';
+export { default as CommandesSearchBar } from './CommandesSearchBar';
+export { default as CommandesTable } from './CommandesTable';
+export { default as CommandesDialogs } from './CommandesDialogs';
+export { default as RdvCreationModal } from './RdvCreationModal';
+export { default as RdvConfirmationModal } from './RdvConfirmationModal';
