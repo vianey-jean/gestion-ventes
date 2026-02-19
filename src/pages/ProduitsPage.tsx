bb94812436783fcb8917c8ae4756cb31e6e0c3f9
Jean Marie Vianey RABEMANALINA <vianey.jean@ymail.com>
2026-02-19 19:45:16 +0400

    mise a jour de la page produits

--- src/pages/ProduitsPage.tsx
+++ src/pages/ProduitsPage.tsx
@@ -1,3 +1,5 @@
+{/** Ajouter les pagionation de la page*/}
+
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Layout from '@/components/Layout';
